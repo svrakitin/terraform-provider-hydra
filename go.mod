77a9fffeb2dc7bf93ce31e115bb954409af71f0b
Mal Curtis <mal@mal.co.nz>
2021-03-09 11:23:56 +1300

    Add BasicAuth support (#2)
    * Add BasicAuth support
    
    * Use structured map for authentication

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/svrakitin/terraform-provider-hydra
 go 1.15
 
 require (
+	github.com/go-openapi/runtime v0.19.26
 	github.com/hashicorp/terraform-plugin-docs v0.4.0
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.4.3
 	github.com/mitchellh/mapstructure v1.4.0
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -2,8 +2,11 @@ package provider
 
 import (
 	"context"
+	"net/http"
 	"net/url"
 
+	"github.com/go-openapi/runtime"
+	httptransport "github.com/go-openapi/runtime/client"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	hydraclient "github.com/ory/hydra-client-go/client"
@@ -20,6 +23,35 @@ func New() *schema.Provider {
 				Type:        schema.TypeString,
 				Required:    true,
 				DefaultFunc: schema.EnvDefaultFunc("HYDRA_ADMIN_URL", nil),
+			},
+			"authentication": {
+				Type:     schema.TypeList,
+				Optional: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"basic": {
+							Type:     schema.TypeList,
+							Optional: true,
+							MaxItems: 1,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"username": {
+										Type:        schema.TypeString,
+										Required:    true,
+										DefaultFunc: schema.EnvDefaultFunc("HYDRA_ADMIN_BASIC_AUTH_USERNAME", nil),
+									},
+									"password": {
+										Type:        schema.TypeString,
+										Required:    true,
+										Sensitive:   true,
+										DefaultFunc: schema.EnvDefaultFunc("HYDRA_ADMIN_BASIC_AUTH_PASSWORD", nil),
+									},
+								},
+							},
+						},
+					},
+				},
 			},
 		},
 		ResourcesMap: map[string]*schema.Resource{
@@ -41,11 +73,36 @@ func providerConfigure(ctx context.Context, data *schema.ResourceData) (interfac
 		return nil, diag.FromErr(err)
 	}
 
-	client := hydraclient.NewHTTPClientWithConfig(nil, &hydraclient.TransportConfig{
+	cfg := &hydraclient.TransportConfig{
 		Schemes:  []string{endpointURL.Scheme},
 		Host:     endpointURL.Host,
 		BasePath: endpointURL.Path,
-	})
+	}
 
+	var transport runtime.ClientTransport
+	if basicAuth, ok := data.GetOk("authentication.0.basic.0"); ok {
+		auth := basicAuth.(map[string]interface{})
+		tr := &BasicAuthTransport{
+			username:     auth["username"].(string),
+			password:     auth["password"].(string),
+			RoundTripper: http.DefaultTransport,
+		}
+		httpClient := &http.Client{Transport: tr}
+		transport = httptransport.NewWithClient(cfg.Host, cfg.BasePath, cfg.Schemes, httpClient)
+	} else {
+		transport = httptransport.New(cfg.Host, cfg.BasePath, cfg.Schemes)
+	}
+
+	client := hydraclient.New(transport, nil)
 	return client.Admin, nil
+}
+
+type BasicAuthTransport struct {
+	username, password string
+	http.RoundTripper
+}
+
+func (ct *BasicAuthTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.SetBasicAuth(ct.username, ct.password)
+	return ct.RoundTripper.RoundTrip(req)
 }
